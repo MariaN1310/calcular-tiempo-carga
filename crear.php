2ee5ed1ea762251b4e210eef7072261b6a33aa2d
Mariano Blanco <blanco.mariano13@gmail.com>
2024-02-12 13:04:29 +0100

    v1 finalizada

--- crear.php
+++ crear.php
@@ -3,52 +3,59 @@
 $tipo = $_POST['tipo'];
 
 if ($tipo == "js") {
+
 	unlink('test.js');
-	$numero = rand(0, 9999);
 
-	$text = "const hrtime".$numero." =
-	(typeof window !== 'undefined' &&
-	typeof window.performance !== 'undefined' &&
-	window.performance.now) ?
-		window.performance.now.bind (window.performance) :
-		(typeof require !== 'undefined') ?
-		require ('perf_hooks').performance.now :
-		function () {
-			var h = process.hrtime".$numero." ();
-			return (h[ 0 ] + (h[ 1 ] / 1e9)) * 1000;
-		};
+	$codeJS = "
+	try {
+		const hrtime =
+		(typeof window !== 'undefined' &&
+		typeof window.performance !== 'undefined' &&
+		window.performance.now) ?
+			window.performance.now.bind (window.performance) :
+			(typeof require !== 'undefined') ?
+			require ('perf_hooks').performance.now :
+			function () {
+				var h = process.hrtime ();
+				return (h[ 0 ] + (h[ 1 ] / 1e9)) * 1000;
+			};
+		var start = hrtime ();
+		
+		".$_POST['text']."
 
-	var start = hrtime".$numero." ();
-
-	";
-
-	$text .= $_POST['text']."
-	
-	var end   = hrtime".$numero." ();
-	var tiempo = (end - start);
-	var tiempo2 = tiempo/1000;
-	console.log ('Tiempo de ejecución:', (end - start).toString().match(/^-?\d+(?:\.\d{0,5})?/)[0], ' milisegundos');
-	$('#tiempo').html('<b>Tiempo de ejecución JS:<br>'+tiempo.toString().match(/^-?\d+(?:\.\d{0,5})?/)[0]+' milisegundos <br>'+tiempo2.toString().match(/^-?\d+(?:\.\d{0,5})?/)[0]+' segundos</b>');";
+		var end   = hrtime ();
+		var tiempo = (end - start);
+		var tiempo2 = tiempo/1000;
+		console.log ('Tiempo de ejecución:', (end - start).toString().match(/^-?\d+(?:\.\d{0,5})?/)[0], ' milisegundos');
+		$('#tiempo').html('<b>Tiempo de ejecución JS:<br>'+tiempo.toString().match(/^-?\d+(?:\.\d{0,5})?/)[0]+' milisegundos <br>'+tiempo2.toString().match(/^-?\d+(?:\.\d{0,5})?/)[0]+' segundos</b>');
+	} catch (error) {
+		console.log ('El codigo tiene un error');
+		$('#tiempo').html('<b>El codigo tiene un error</b>');
+	}";
 
 	$file = fopen("test.js","a");
-	fputs($file, $text) or die("No se puedo crear el Fichero.");
+	fputs($file, $codeJS) or die("No se puedo crear el Fichero.");
 	fclose($file);
 
-	echo $text;
+	echo $codeJS;
+
 } elseif ($tipo == "php") {
+
 	unlink('test.php');
-	$text = "<?php
+	
+	$codePHP = "<?php
 	\$start = hrtime(true);
 	";
-	$text .= $_POST['text']."
+	$codePHP .= $_POST['text']."
 	";
 
-	$text .= "\$end = hrtime(true);
-	\$time = \$end-\$start;
-	echo \$time/1e+6; ?>";
+	$codePHP .= "\$end = hrtime(true);
+	
+	echo (\$end-\$start)/1e+6; ?>";
 
 	$file = fopen("test.php","a");
-	fputs($file, $text) or die("No se puedo crear el Fichero.");
+	fputs($file, $codePHP) or die("No se puedo crear el Fichero.");
 	fclose($file);
+
 }
 ?>
